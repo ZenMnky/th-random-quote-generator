4939dbcbb7ac42547b30fa7402cc0689e96322fa
Justin Hager <coachjustinhager@gmail.com>
2020-03-19 15:49:05 -0700

    Add quote object, getRandomQuote(), printQuote()

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # th-random-quote-generator
+TreeHouse Full Stack JavaScript Unit 1 Project
+
+## objectives
+1 - Build a collection of quotations
+    a. Each quote object requires the following properties: quote, source, citation (optional), year (optional)
+2 - Create a function that pulls one of those quotes out at random
+3 - Create a function that prints that quote to the screen
+
--- js/script.js
+++ js/script.js
@@ -10,19 +10,85 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: `Hold sacred your capacity for understanding. For in it is all, that our ruling principle won't allow anything to enter that is either inconsistent with nature or with the constitution ofa logical reature. It's what demands due diligence, care for others, and obedience to God.`,
+    source: 'Marcus Aurelius',
+    citation: 'Meditations, 3.9',
+    year: '121-180 AD'
+  },
+  {
+    quote: `You are not your body and hairstyle, but your capacity for choosing well. If your choices are beautiful, so too will you be`,
+    source: 'Epictetus',
+    citation: 'Discourses, 3.1.39b-40a',
+    year: '1535 AD'
+  },
+  {
+    quote: 'Good',
+    source: 'Jocko Willink',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: `Today I escaped from the crush of circumstances, or better put, I threw them out, for the crush wasn't from otside me but in my own assumptions.`,
+    source: 'Marcus Aurelius',
+    citation: 'Meditations, 9.13',
+    year: '121-180 AD'
+  },
+  {
+    quote: `If you do not listen to that thing that beckons you forward, you will pay for it like you cannot possibly imagine.`,
+    source: 'Dr. Jordan B. Peterson, PhD',
+    citation: `Biblical Series IX: The Call to Abraham (YouTube)`,
+    year: '2017'
+  },
+  {
+    quote: `The cause of my irritation is not in this person but in me`,
+    source: 'Anthony de Mello',
+    citation: `The Way to Love`,
+    year: '1995'
+  }
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  const upperLimit = quotes.length;
+  const randomNumber = Math.floor(Math.random() * upperLimit + 1)
+  return quotes[randomNumber];
+}
 
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() {
+  let newQuoteObj = getRandomQuote();
+  let quote = document.querySelector('p.quote');
+  let source = document.querySelector('p.source');
+     
+  function buildSourceHTML() {
+    let sourceHTML = '';
+    sourceHTML = `${newQuoteObj.source}`;
+
+    if (newQuoteObj.citation != '') {
+      sourceHTML += `
+      <span class="citation">${newQuoteObj.citation}</span>
+      `;
+    }
+    if (newQuoteObj.year != ''){
+      sourceHTML += `
+      <span class="citation">${newQuoteObj.year}</span>
+      `;
+    }
+    return sourceHTML;
+  }
+
+  quote.innerHTML = newQuoteObj.quote;
+  source.innerHTML = buildSourceHTML();  
+}
 
 
 /***
