abdc9acfa94219d2b9272fe42805388233ebac33
Justin Hager <coachjustinhager@gmail.com>
2020-03-20 10:16:47 -0700

    Fixes Issue #4 - BG Colors

--- js/script.js
+++ js/script.js
@@ -2,17 +2,8 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /**
- * Still to do:
- * + Add 'tag' property to objects
- * + Display tag properties
-  * + Auto refresh
-  * + Update comments
+ * Going for 'Exceeds Expectations' Requirements. Please reject if it does not meet the requirements.
  */
 
 /*** 
@@ -23,44 +14,53 @@ const quotes = [
     quote: `Hold sacred your capacity for understanding. For in it is all, that our ruling principle won't allow anything to enter that is either inconsistent with nature or with the constitution ofa logical reature. It's what demands due diligence, care for others, and obedience to God.`,
     source: 'Marcus Aurelius',
     citation: 'Meditations, 3.9',
-    year: '121-180 AD'
+    year: '121-180 AD',
+    tags: ['stoicism']
   },
   {
     quote: `You are not your body and hairstyle, but your capacity for choosing well. If your choices are beautiful, so too will you be`,
     source: 'Epictetus',
     citation: 'Discourses, 3.1.39b-40a',
-    year: '1535 AD'
+    year: '1535 AD',
+    tags: ['stoicism']
   },
   {
     quote: 'Good',
     source: 'Jocko Willink',
     citation: '',
-    year: ''
+    year: '',
+    tags: ['growth_mindset']
   },
   {
     quote: `Today I escaped from the crush of circumstances, or better put, I threw them out, for the crush wasn't from outside me but in my own assumptions.`,
     source: 'Marcus Aurelius',
     citation: 'Meditations, 9.13',
-    year: '121-180 AD'
+    year: '121-180 AD',
+    tags: ['stoicism']
   },
   {
     quote: `If you do not listen to that thing that beckons you forward, you will pay for it like you cannot possibly imagine.`,
     source: 'Dr. Jordan B. Peterson, PhD',
     citation: `Biblical Series IX: The Call to Abraham (YouTube)`,
-    year: '2017'
+    year: '2017',
+    tags: ['JBP', 'meaning']
   },
   {
     quote: `The cause of my irritation is not in this person but in me`,
     source: 'Anthony de Mello',
     citation: `The Way to Love`,
-    year: '1995'
+    year: '1995',
+    tags: ['stoicism']
   }
 ]
 
 /**
  * Background Colors Array
+ * 
+ * Green, Blue, Auburn, Lighter Auburn, Red, Natural Paper
  */
-const bgColors = ['#30e849', '#30c9e8', '#c79816', '#e86830', '#e83a30']
+const bgColors = ['#30e849', '#30c9e8', '#c75716', '#e86830', '#e83a30', '#f4c89c']
+
 
 /**
  * Random BG Color function
@@ -112,12 +112,12 @@ function printQuote() {
   source.innerHTML = buildSourceHTML();
   setRandomBgColor();
  
+}
 
-   /**
+/**
     * Auto-refresh quotes at regular intervals
     */
 
-}
 
 
 
